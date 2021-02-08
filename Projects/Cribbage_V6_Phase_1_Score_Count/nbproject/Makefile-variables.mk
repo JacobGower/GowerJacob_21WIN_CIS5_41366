16e5b01eeac53653dc679101aabe8122931bf117
JacobGower <jgower@student.rccd.edu>
2021-02-07 21:28:33 -0800

    Signed-off-by: JacobGower <jgower@student.rccd.edu>

---
+++ Projects/Cribbage_V6_Phase_1_Score_Count/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=cribbage_v6_phase_1_score_count
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/cribbage_v6_phase_1_score_count
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=cribbagev6phase1scorecount.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/cribbagev6phase1scorecount.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=cribbage_v6_phase_1_score_count
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/cribbage_v6_phase_1_score_count
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=cribbagev6phase1scorecount.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/cribbagev6phase1scorecount.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
